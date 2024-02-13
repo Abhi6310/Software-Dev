7d9de7d6038e7367a0c974023c8eaf1a80d6fe5f
Shipra Singh <142837989+shipradsingh@users.noreply.github.com>
2024-02-12 22:43:17 -0700

    Create index.js in part_b

---
+++ part_b/index.js
@@ -0,0 +1,69 @@
+// ************************************************
+// <!-- Section 1 : Dependencies-->
+// ************************************************
+
+// importing the dependencies
+// Express is a NodeJS framework that, among other features, allows us to create HTML templates.
+const express = require('express');
+const bodyParser = require('body-parser');
+const pgp = require('pg-promise')();
+require('dotenv').config();
+
+// ************************************************
+// <!-- Section 2 : Initialization-->
+// ************************************************
+
+// defining the Express app
+const app = express();
+// using bodyParser to parse JSON in the request body into JS objects
+app.use(bodyParser.json());
+// Database connection details
+const dbConfig = {
+  host: 'db',
+  port: 5432,
+  database: process.env.POSTGRES_DB,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+};
+// Connect to database using the above details
+const db = pgp(dbConfig);
+
+
+// ************************************************
+// <!-- Section 3 : All Endpoint Implementations -->
+// ************************************************
+
+// GET Endpoint Implementation (Default)
+// <!-- Endpoint 1 :  Default endpoint ("/") -->
+const message = 'Hey there!';
+app.get('/', (req, res) => {
+  res.send(message);
+});
+
+// GET Endpoint Implementation
+// <!-- Endpoint 2 :   -->
+app.get('', function (req, res) {
+});
+
+// POST Endpoint Implementation
+// <!-- Endpoint 3  -->
+app.post('', function (req, res) {
+});
+
+// PUT Endpoint Implementation
+// <!-- Endpoint 4  -->
+app.put('', function (req, res) {
+});
+
+// DELETE Endpoint Implementation
+// <!-- Endpoint 5  -->
+app.delete('', function (req, res) {
+});
+
+// ************************************************
+// <!-- Section 4 : Start Server-->
+// ************************************************
+// starting the server and keeping the connection open to listen for more requests
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
